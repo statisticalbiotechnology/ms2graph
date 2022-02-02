3e3d95fdd2aa90cc0adc68b6b5f2d3dbd1b8885f
percolator <lukas.kall@scilifelab.se>
2022-02-02 21:55:32 +0100

    moving

--- python/ms2graph.py
+++ python/ms2graph.py
@@ -23,7 +23,9 @@ fragment_tol_mode = 'ppm'
 
 single_aa_mass = {aa[0]:comp.mass() for aa, comp in mass.std_aa_comp.items() if len(aa)==1 and aa != 'I'}
 double_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in single_aa_mass.items() for aa2, aam2 in single_aa_mass.items()}
-std_aa_mass = dict(sorted({ **single_aa_mass, **double_aa_mass}.items(), key=lambda item: item[1]))
+tripple_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in double_aa_mass.items() for aa2, aam2 in single_aa_mass.items()}
+quad_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in double_aa_mass.items() for aa2, aam2 in double_aa_mass.items()}
+std_aa_mass = dict(sorted({ **single_aa_mass, **double_aa_mass, **tripple_aa_mass, **quad_aa_mass}.items(), key=lambda item: item[1]))
 #std_aa_mass = dict(sorted(single_aa_mass.items(), key=lambda item: item[1]))
 aa_mass = array('d',std_aa_mass.values())
 aa_ix = list(std_aa_mass.keys())
@@ -56,18 +58,18 @@ def find_diff(difference, tolerance, aa_masses = aa_mass, aa_index = aa_ix):
     return None
     
 
-def generate_graph(mzarray, precursor_mass, n_term_mass=n_term_mass, c_term_mass=c_term_mass):
+def generate_graph(mzarray, precursor_mass, n_mass=n_term_mass, c_mass=c_term_mass):
     #print(mzarray)
-    mzarray = np.insert(mzarray, 0, c_term_mass)
-    mzarray = np.insert(mzarray, 0, n_term_mass)
-    mzarray = np.append(mzarray, [precursor_mass + n_term_mass])
-    mzarray = np.append(mzarray, [precursor_mass + c_term_mass])
+    mzarray = np.insert(mzarray, 0, c_mass)
+    mzarray = np.insert(mzarray, 0, n_mass)
+    mzarray = np.append(mzarray, [precursor_mass + n_mass])
+    mzarray = np.append(mzarray, [precursor_mass + c_mass])
     _num_peaks = mzarray.shape[0]
     _from, _to, _edge_aa = [], [], []
     for i in range(_num_peaks-1):
         for j in range(i+1,_num_peaks):
             _diff = mzarray[j] - mzarray[i]
-            _aa = find_diff(_diff, 0.02)
+            _aa = find_diff(_diff, fragment_tol_mass*mzarray[j]/1.E6)
             if _aa == False:
                 break
             if _aa:
@@ -92,6 +94,34 @@ def brute_force(f,t,c,goto=0,peptide=""):
                 return
             brute_force(f,t,c,_to,_current_peptide)
 
+def right_path(peptide,f,t,c):
+    _b_max = list(set(t))[-2] # last b-ion
+    _b_loc, _y_loc = 0, list(set(t))[-1]  # initiating to first b- and last y-ion
+    # matching forward
+    for _pos in range(len(peptide)-1):
+        residues = peptide[_pos:min(_pos+4,len(peptide))]
+        _b_indeces = [i for i, x in enumerate(f) if x == _b_loc]
+        _b_to = [t[_ix] for _ix in _b_indeces if residues.startswith(c[_ix])]
+        if len(_b_to)==0:
+            print(f"No match for b pos={_pos}, residues={residues}")
+            break
+        print("b matches ", [c[_ix] for _ix in _b_indeces if residues.startswith(c[_ix])])
+        _b_loc = _b_to[0] # Take the first (lightest)
+    # matching backward
+    for _pos in range(len(peptide),0,-1):
+        residues = peptide[max(0,_pos-4):_pos]
+        _y_indeces = [i for i, x in enumerate(t) if x == _y_loc]
+        if len(_y_indeces)==0:
+            print(f"No start of a match for y pos={_pos}, residues={residues}")
+            break
+        print([c[_ix] for _ix in _y_indeces])
+        _y_from = [f[_ix] for _ix in _y_indeces if c[_ix] and residues.endswith(c[_ix])]
+        if len(_y_from)==0:
+            print(f"No match for y pos={_pos}, residues={residues}")
+            break
+        print("y matches ", [c[_ix] for _ix in _y_indeces if residues.endswith(c[_ix])])
+        _y_loc = _y_from[-1] # Take the last (lightest)
+
     
 
 #print(mass.std_aa_comp)
@@ -106,31 +136,25 @@ with mz.read(mzFile) as spectra:
                 p_mz = float(precursor['selected ion m/z'])
                 p_z = int(precursor['charge state'])
                 p_m = (p_mz-mass.Composition({'H+': 1}).mass())*p_z
-                #print(spectrum)
                 print("Spectrum {0}, MS level {ms_level} @ RT {scan_time:1.2f}, z={z}, precursor m/z={mz:1.2f} mass={mass:1.2f}".format(
                     spectrum["id"], ms_level=spectrum["ms level"], scan_time=spectrum["scanList"]["scan"][0]["scan start time"], 
                     z=p_z, mz=p_mz, mass=p_m ))
                 mzarray = spectrum['m/z array']
                 peaks = mzarray.copy()
-                _from, _to, _edge_aa = generate_graph(mzarray, p_m)
-                brute_force(_from, _to, _edge_aa)
-                # print(p_m)
+                # Plot spectrum with annotation
                 annotated_spectrum = sus.MsmsSpectrum("my_spectrum", p_mz, p_z,
                             peaks, spectrum['intensity array'])
-                            # peptide=psmPeptide)
-                # Process the MS/MS spectrum.
                 annotated_spectrum = (annotated_spectrum.set_mz_range(min_mz=100, max_mz=1400)
                     .remove_precursor_peak(fragment_tol_mass, fragment_tol_mode)
                     .filter_intensity(min_intensity=0.05, max_num_peaks=50)
                     .scale_intensity('root')
                     .annotate_proforma(proforma_str=psmPeptide,fragment_tol_mass=fragment_tol_mass, fragment_tol_mode=fragment_tol_mode, ion_types='by'))
-                # Plot the MS/MS spectrum.
-                #fig, ax = plt.subplots(figsize=(12, 6))
-                #sup.spectrum(annotated_spectrum, ax=ax)
-                #plt.show()
-                #plt.close()            
                 (sup.spectrum(annotated_spectrum).properties(width=640, height=400)
                        .save('spectrum_iplot.html'))
+                _from, _to, _edge_aa = generate_graph(mzarray, p_m, n_term_mass, c_term_mass)
+                right_path(psmPeptide, _from, _to, _edge_aa)
+                # brute_force(b_from, b_to, b_edge_aa)
+                # print(p_m)
 
 print(mass.fast_mass(psmPeptide[:1], ion_type='b', charge=1))
 print(mass.fast_mass(psmPeptide[:2], ion_type='b', charge=1))
