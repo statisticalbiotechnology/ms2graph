a80152c11ac3c06388e8c91617cae4218e306063
codicef <francesco.codice@gmail.com>
2022-02-10 16:08:04 +0100

    Spectrum/PyG Class implemented in data.py, minor changes to ms2graph.py for modularity purposes

---
+++ python/data.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+import torch
+from torch_geometric.data import Data
+from torch_geometric import utils
+import networkx as nx
+from ms2graph import process_sample_spectrum
+import pyteomics.mzml as mz
+import pyteomics.mass as mass
+from matplotlib import pyplot as plt
+import seaborn as sns
+import numpy as np
+
+
+class Spectrum(object):
+    """
+    Class containing Spectrum data.
+
+    Attributes
+    ----------
+    idx : int
+        Spectrum scan index
+    graph : torch_geometric.data.Data
+        Graph in pyG format. It contains mainly node features and edges
+        definitions.
+    intensities : np.array
+        Numpy array containing intensities values for each peak
+    mz_array : np.array
+        Numpy array containing mass to charge values of the mass spectrum
+    precursor_data : dict
+        Dictionary containing info about precursor (mass, charge, m/z)
+        keys{'m': ,'z':, 'mz }
+    peptide_seq : str, optional
+        Peptide sequence corresponding to spectrum
+
+    Methods
+    -------
+    show_spectrum(out='../tmp/spectrum_plot_{idx}.html'.format(idx=self.idx))
+        Save spectrum plot on a html file. Relies on spectrum_utils package.
+    show_graph()
+        Display a graph representation using NetworkX (network analysis package
+        for python)
+    print_stats()
+        Print graph and spectrum statistics
+
+    """
+
+    def __init__(self, data):
+        self.idx = data['idx']
+        self.mz_array = data['mz_array']
+
+        self.intensities = data['intensities']
+        self.intensities = np.insert(self.intensities, 0, 0)  # c term
+        self.intensities = np.insert(self.intensities, 0, 0)  # n term
+        self.intensities = np.append(self.intensities, 0)  # precursor + c term
+        self.intensities = np.append(self.intensities, 0)  # precursor + n term
+
+        self.precursor = {'m': data['p_m'], 'z': data['p_z'], 'mz':
+                          data['p_mz']}
+        self.edges_aa = data['edges_aa']
+        if 'peptide_seq' in data.keys():
+            self.peptide_seq = data['peptide_seq']
+        self.graph = self.__generate_pyg_graph(data['from'], data['to'],
+                                               data['edges_aa'])
+
+    def __generate_pyg_graph(self, _from, _to, _edges, use_intensities=True):
+        """
+        Encodes _from, _to, _edges lists into a torch_geometric.data.Data class instance.
+
+        Parameters
+        ----------
+        _from : list of int
+            List of nodes indices
+        _to : list of int
+            List of nodes indices
+        _edges : list of str
+            List of amino acids corresponding to each edge
+        use_intensities : bool
+            Boolean parameter to either consider intensities as node features,
+            should be present in Spectrum instance.
+
+        Returns
+        -------
+        graph : torch_geometric.data.Data
+            Pytorch geometric graph class instance
+        """
+        assert len(_from) == len(_to) == len(_edges)
+        num_nodes = len(self.mz_array)
+        edge_index = torch.tensor([_from, _to], dtype=torch.float)
+
+        if use_intensities and self.intensities is not None:
+            assert len(self.intensities) == num_nodes
+            x = torch.tensor(self.intensities, dtype=torch.float)
+        else:
+            x = torch.tensor(range(num_nodes), dtype=torch.float)
+
+        graph = Data(x=x, edge_index=edge_index)
+        return graph
+
+    def show_graph(self):
+        """
+        Show graph structure using NetworkX.
+
+        """
+        assert self.graph is not None
+        nx_graph = utils.to_networkx(self.graph,
+                                     to_undirected=False)
+        node_attributes = {key: round(value, 2) for (key, value) in
+                           enumerate(list(self.intensities))}
+
+        # edge_attributes missing because of many overlapping edges
+
+        nx_graph = nx.relabel_nodes(nx_graph, node_attributes)
+        pos = nx.kamada_kawai_layout(nx_graph)
+        nx.draw_networkx(nx_graph, pos=pos)
+        plt.show()
+
+    def print_stats(self):
+        """
+        Print basic information about the graph.
+
+        (nodes number, edges number, isolated nodes, directed or not)
+        """
+        num_info = "{title:17} = {value:5d}"
+        float_info = "{title:17} = {value:3.2f}"
+        bool_info = "{title:17} = {value:5}"
+
+        print(num_info.format(title="Nodes Number",
+              value=self.graph.num_nodes))
+        print(num_info.format(title="Edges Number",
+              value=self.graph.num_edges))
+        print(num_info.format(title="Isolated Nodes",
+              value=self.graph.has_isolated_nodes()))
+        print(float_info.format(title="Avg Node Degree",
+                               value=self.graph.num_edges / self.graph.num_nodes))
+        print(bool_info.format(title="Directed",
+                               value=str(self.graph.is_directed())))
+
+
+if __name__ == '__main__':
+    # Testing of module classes instances
+
+    # Graph Spectrum instance test
+    mzFile = "../data/converted/LFQ_Orbitrap_DDA_Yeast_01.mzML"
+    mzScan = 32688
+    psmPeptide = "IANVQSQLEK"
+    precursorCharge = 2
+    print(f"Recreate {psmPeptide} with mass {mass.calculate_mass(psmPeptide):1.2f}")
+    fragment_tol_mass = 10
+    fragment_tol_mode = 'ppm'
+    data = process_sample_spectrum(mzScan, mzFile, False,
+                                   fragment_tol_mass,
+                                   fragment_tol_mode)
+    sample_spectrum = Spectrum(data)
+    sample_spectrum.print_stats()
+    sample_spectrum.show_graph()
+
+    pass
--- python/ms2graph.py
+++ python/ms2graph.py
@@ -11,21 +11,20 @@ import urllib.parse
 
 from bisect import *
 from array import *
-percolatorFile = "../data/crux-output/percolator.target.psms.txt"
-mzFile = "../data/converted/LFQ_Orbitrap_DDA_Yeast_01.mzML"
-mzScan = 32688
-psmPeptide = "IANVQSQLEK"
-precursorCharge = 2
 
-print(f"Recreate {psmPeptide} with mass {mass.calculate_mass(psmPeptide):1.2f}") 
+# Global Variables
+percolatorFile = "../data/crux-output/percolator.target.psms.txt"
 fragment_tol_mass = 10
 fragment_tol_mode = 'ppm'
-
 single_aa_mass = {aa[0]:comp.mass() for aa, comp in mass.std_aa_comp.items() if len(aa)==1 and aa != 'I'}
 double_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in single_aa_mass.items() for aa2, aam2 in single_aa_mass.items()}
 tripple_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in double_aa_mass.items() for aa2, aam2 in single_aa_mass.items()}
-quad_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in double_aa_mass.items() for aa2, aam2 in double_aa_mass.items()}
-std_aa_mass = dict(sorted({ **single_aa_mass, **double_aa_mass, **tripple_aa_mass, **quad_aa_mass}.items(), key=lambda item: item[1]))
+
+# Quad aa masses makes tha graph probably too dense TODO to be discussed
+# quad_aa_mass = {aa1+aa2:aam1+aam2 for aa1, aam1 in double_aa_mass.items() for aa2, aam2 in double_aa_mass.items()}
+# std_aa_mass = dict(sorted({ **single_aa_mass, **double_aa_mass, **tripple_aa_mass, **quad_aa_mass}.items(), key=lambda item: item[1]))
+
+std_aa_mass = dict(sorted({ **single_aa_mass, **double_aa_mass, **tripple_aa_mass}.items(), key=lambda item: item[1]))
 #std_aa_mass = dict(sorted(single_aa_mass.items(), key=lambda item: item[1]))
 aa_mass = array('d',std_aa_mass.values())
 aa_ix = list(std_aa_mass.keys())
@@ -61,7 +60,8 @@ def find_diff(difference, tolerance, aa_masses = aa_mass, aa_index = aa_ix):
     return aa_index[ixlo:ixhigh]
     
 
-def generate_graph(mzarray, precursor_mass, series_charge=1):
+def generate_graph(mzarray, precursor_mass, series_charge=1,
+                   fragment_tol_mass=fragment_tol_mass, verbose=False):
     #print(mzarray)
     n_term_mass = mass.Composition({'H+': series_charge}).mass()
     c_term_mass = mass.Composition({'O': 1, 'H': 2, 'H+': series_charge}).mass()
@@ -79,12 +79,14 @@ def generate_graph(mzarray, precursor_mass, series_charge=1):
             if _aas == None:
                 break
             for _aa in _aas:
-                _previous = [_cl for _cl in _claimed if _aa.beginswith(_cl)]
-                if len(_previous) ==0:
+                _previous = [_cl for _cl in _claimed if _aa.startswith(_cl)]
+                if len(_previous) == 0:
                     _from.append(i)
                     _to.append(j)
                     _edge_aa.append(_aa)
-                    #print("Peaks {0}, {1} have a mass diff of {2:1.3f}, i.e. a {3} of mass {4:1.3f}".format(mzarray[i], mzarray[j], _diff, chr(_aa), std_aa_mass[aa] ))
+                    _claimed.append(_aa)  # TODO to be checked
+                    if verbose:
+                        print("Peaks {0}, {1} have a mass diff of {2:1.3f}, i.e. a {3} of mass {4:1.3f}".format(mzarray[i], mzarray[j], _diff, _aa, std_aa_mass[_aa] ))
     return _from, _to, _edge_aa, mzarray
 
 
@@ -131,6 +133,7 @@ def right_path(peptide, f, t, c, orientation, peaks):
         _loc = _to[0] # Take the first (lightest)
         _ion_nr += len(_char[0]) # move forward according to length of match
 
+
 def processSpectra(mzFile, scan2charge, scan2peptide):
     with mz.read(mzFile) as spectra:
         for spectrum in spectra:
@@ -153,39 +156,80 @@ def processSpectra(mzFile, scan2charge, scan2peptide):
                     right_path(psmPeptide, _from, _to, _edge_aa, -1, peaks)
 
 
-scan2charge, scan2peptide = readPSMs(percolatorFile, specFileIndex="0", fdrTreshold=0.01)
-processSpectra(mzFile, scan2charge, scan2peptide)
-quit()
+def process_sample_spectrum(mzScan, mzFile, plot_spectrum=True,
+                            fragment_tol_mass=fragment_tol_mass,
+                            fragment_tol_mode=fragment_tol_mode):
+    # Well working example spectrum
+    with mz.read(mzFile) as spectra:
+        for spectrum in spectra:
+            # print (spectrum)
+            # print (spectrum["scanList"]["scan"][0])
+            if spectrum["ms level"]==2:
+                if spectrum["index"]==mzScan-1:
+                    precursor = spectrum["precursorList"]['precursor'][0]['selectedIonList']['selectedIon'][0]
+                    p_mz = float(precursor['selected ion m/z'])
+                    p_z = int(precursor['charge state'])
+                    p_m = (p_mz-mass.Composition({'H+': 1}).mass())*p_z
+                    print("Spectrum {0}, MS level {ms_level} @ RT {scan_time:1.2f}, z={z}, precursor m/z={mz:1.2f} mass={mass:1.2f}".format(
+                        spectrum["id"], ms_level=spectrum["ms level"], scan_time=spectrum["scanList"]["scan"][0]["scan start time"],
+                        z=p_z, mz=p_mz, mass=p_m ))
+                    mzarray = spectrum['m/z array']
+                    peaks = mzarray.copy()
 
-# Well working example spectrum
-with mz.read(mzFile) as spectra:
-    for spectrum in spectra:
-        # print (spectrum)
-        # print (spectrum["scanList"]["scan"][0])
-        if spectrum["ms level"]==2:
-            if spectrum["index"]==mzScan-1:
-                precursor = spectrum["precursorList"]['precursor'][0]['selectedIonList']['selectedIon'][0]
-                p_mz = float(precursor['selected ion m/z'])
-                p_z = int(precursor['charge state'])
-                p_m = (p_mz-mass.Composition({'H+': 1}).mass())*p_z
-                print("Spectrum {0}, MS level {ms_level} @ RT {scan_time:1.2f}, z={z}, precursor m/z={mz:1.2f} mass={mass:1.2f}".format(
-                    spectrum["id"], ms_level=spectrum["ms level"], scan_time=spectrum["scanList"]["scan"][0]["scan start time"], 
-                    z=p_z, mz=p_mz, mass=p_m ))
-                mzarray = spectrum['m/z array']
-                peaks = mzarray.copy()
-                # Plot spectrum with annotation
-                annotated_spectrum = sus.MsmsSpectrum("my_spectrum", p_mz, p_z,
-                            mzarray, spectrum['intensity array'])
-                annotated_spectrum = (annotated_spectrum.set_mz_range(min_mz=100, max_mz=1400)
-                    .remove_precursor_peak(fragment_tol_mass, fragment_tol_mode)
-                    .filter_intensity(min_intensity=0.05, max_num_peaks=50)
-                    .scale_intensity('root')
-                    .annotate_proforma(proforma_str=psmPeptide,fragment_tol_mass=fragment_tol_mass, fragment_tol_mode=fragment_tol_mode, ion_types='by'))
-                (sup.spectrum(annotated_spectrum).properties(width=640, height=400)
-                       .save('spectrum_iplot.html'))
-                _from, _to, _edge_aa, peaks = generate_graph(peaks, p_m, n_term_mass, c_term_mass)
-                right_path(psmPeptide, _from, _to, _edge_aa, 1, peaks)
-                right_path(psmPeptide, _from, _to, _edge_aa, -1, peaks)
-                # right_path(psmPeptide, _from, _to, _edge_aa, peaks)
-                # brute_force(b_from, b_to, b_edge_aa)
-                # print(p_m)
+                    # Filter peaks [TODO to be checked]
+                    annotated_spectrum = sus.MsmsSpectrum("my_spectrum", p_mz, p_z,
+                                                          mzarray, spectrum['intensity array'])
+                    annotated_spectrum = (annotated_spectrum.set_mz_range(min_mz=100, max_mz=1400)
+                                          .filter_intensity(min_intensity=0.05, max_num_peaks=50)
+                                          .scale_intensity('root'))
+
+                    _from, _to, _edge_aa, peaks = generate_graph(annotated_spectrum.mz,
+                                                                 p_m, series_charge=1,
+                                                                 fragment_tol_mass=fragment_tol_mass,
+                                                                 verbose=False)
+                    if plot_spectrum:
+                        # Plot spectrum with annotation
+                        annotated_spectrum = (annotated_spectrum
+                                              .remove_precursor_peak(fragment_tol_mass,
+                                                                     fragment_tol_mode)
+                                              .annotate_proforma(
+                                                  proforma_str=psmPeptide,
+                                                  fragment_tol_mass=fragment_tol_mass,
+                                                  fragment_tol_mode=fragment_tol_mode,
+                                                  ion_types='by'))
+                        (sup.spectrum(annotated_spectrum).properties(width=640, height=400)
+                         .save('spectrum_iplot.html'))
+                        right_path(psmPeptide, _from, _to, _edge_aa, 1, peaks)
+                        right_path(psmPeptide, _from, _to, _edge_aa, -1, peaks)
+                        # right_path(psmPeptide, _from, _to, _edge_aa, peaks)
+                        # brute_force(b_from, b_to, b_edge_aa)
+                        # print(p_m)
+                    data = {'idx': spectrum['index'],
+                            'mz_array': peaks,
+                            'intensities': annotated_spectrum.intensity,
+                            'p_m': p_m,
+                            'p_z': p_z,
+                            'p_mz': p_mz,
+                            'from': _from,
+                            'to': _to,
+                            'edges_aa': _edge_aa}
+                    return data
+
+
+if __name__ == '__main__':
+    # Process sample spectrum
+    print("Processing sample spectrum")
+
+    mzFile = "../data/converted/LFQ_Orbitrap_DDA_Yeast_01.mzML"
+    mzScan = 32688
+    psmPeptide = "IANVQSQLEK"
+    precursorCharge = 2
+
+    print(f"Recreate {psmPeptide} with mass {mass.calculate_mass(psmPeptide):1.2f}")
+    process_sample_spectrum(mzScan, mzFile)
+    quit()
+
+    # Process multiple spectra
+    scan2charge, scan2peptide = readPSMs(percolatorFile, specFileIndex="0", fdrTreshold=0.01)
+    processSpectra(mzFile, scan2charge, scan2peptide)
+    quit()
